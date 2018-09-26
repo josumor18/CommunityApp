357f3d30a11cf2b0a9bc67f19589b2fa8c2c37d4
Carlos Josué Morales R <carlosjosum@hotmail.com>
2018-09-25 19:12:13 -0600

    Login without AcctionBar

--- app/src/main/java/elcarmen/project/community/Business/LoginAcivity.java
+++ app/src/main/java/elcarmen/project/community/Business/LoginAcivity.java
@@ -11,6 +11,7 @@ import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -47,6 +48,8 @@ public class LoginAcivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        getSupportActionBar().hide();
 
         rlLogin = findViewById(R.id.rlLogin);
         rlLoginPB = findViewById(R.id.rlLoginPB);
