04729e66ebd2219aa9f52e620ccc570936e66136
Diego Armando Guzmán Villalta <33551960+Diego-Guzman@users.noreply.github.com>
2018-10-01 21:11:20 -0600

    added new features to notifications

--- app/src/main/java/elcarmen/project/community/Business/NotificationsFragment.java
+++ app/src/main/java/elcarmen/project/community/Business/NotificationsFragment.java
@@ -6,11 +6,10 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.NotificationCompatSideChannelService;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
@@ -57,24 +56,13 @@ public class NotificationsFragment extends Fragment {
 
 
 
-        rlNotifications = v.findViewById(R.id.rlNitifications);
+        rlNotifications = v.findViewById(R.id.rlNotifications);
         lvNotifications = v.findViewById(R.id.lvNotifications);
-        lvNotifications.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                /*  REDIRECCIONAR A DETALLES SEGUN TIPO DE NOTIFICACION
-                Intent intent = new Intent(getActivity(), CommunityActivity.class);
-                int idCommunity_enviar = communities.get(position).getId();
-                String nameCommunity_enviar = communities.get(position).getName();
-                intent.putExtra("idCommunity",idCommunity_enviar);
-                intent.putExtra("nameCommunity", nameCommunity_enviar);
-                startActivity(intent);
-                */
-            }
-        });
 
 
-        NotificationsFragment.ExecuteGetNotifications executeGetNotifications = new NotificationsFragment.ExecuteGetNotifications();
+
+        ExecuteGetNotifications executeGetNotifications = new
+                                                    ExecuteGetNotifications();
         executeGetNotifications.execute();
 
         return v;
@@ -89,36 +77,32 @@ public class NotificationsFragment extends Fragment {
             User_Singleton.getInstance().setAuth_token(token);
             LoginAcivity.actualizarAuth_Token(token, getActivity());
 
-            /*
-            JSONArray jsonCommunitiesList = jsonResult.getJSONArray("communities");
-            for (int i = 0; i < jsonCommunitiesList.length(); i++) {
-                JSONObject jsonCommunity = (JSONObject) jsonCommunitiesList.get(i);
 
-                JSONArray jsonRules = jsonCommunity.getJSONArray("rules");
-                ArrayList<String> rules = new ArrayList<String>();
-                for(int ind = 0; ind < jsonRules.length(); ind++){
-                    rules.add(jsonRules.getString(ind));
-                }
+            JSONArray jsonNotificationsList = jsonResult.getJSONArray("notifications");
+            for (int i = 0; i < jsonNotificationsList.length(); i++) {
+                JSONObject jsonNotification = (JSONObject) jsonNotificationsList.get(i);
 
-                JSONArray jsonSubs = jsonCommunity.getJSONArray("sub_communities");
-                ArrayList<Integer> subs = new ArrayList<Integer>();
-                for(int ind = 0; ind < jsonSubs.length(); ind++){
-                    subs.add(jsonSubs.getInt(ind));
-                }
+                Notification notification = new Notification(jsonNotification.getInt("id"),
+                        jsonNotification.getInt("idUser"),
+                        jsonNotification.getInt("idContent"),
+                        jsonNotification.getBoolean("isNews"),
+                        jsonNotification.getBoolean("isReports"),
+                        jsonNotification.getBoolean("isEvents"),
+                        jsonNotification.getString("titleContent"),
+                        jsonNotification.getBoolean("seen"),
+                        jsonNotification.getString("photo"));
 
-                Community community = new Community(jsonCommunity.getInt("id"), jsonCommunity.getString("name"), jsonCommunity.getString("description"), rules, jsonCommunity.getBoolean("isSubcommunity"), jsonCommunity.getString("photo"), jsonCommunity.getString("photo_thumbnail"), subs);
-
-                communities.add(community);
+                notifications.add(notification);
             }
-            */
+
         } catch (JSONException e) {
             e.printStackTrace();
         }
 
-        lvNotifications.setAdapter(new NotificationsFragment.NotificationsAdapter());
+        lvNotifications.setAdapter(new NotificationsAdapter());
     }
 
-    /////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////
     public class NotificationsAdapter extends BaseAdapter {
 
         public NotificationsAdapter() {
@@ -141,16 +125,16 @@ public class NotificationsFragment extends Fragment {
         }
 
         @Override
-        public View getView(int i, View view, ViewGroup viewGroup) {
+        public View getView(final int i, View view, ViewGroup viewGroup) {
             LayoutInflater inflater = getLayoutInflater();
             if (view == null) {
                 view = inflater.inflate(R.layout.notifications_list_item, null);
             }
+            view.setBackgroundColor(getResources().getColor(R.color.colorPrimaryLight));
 
             ImageView imgNotif = view.findViewById(R.id.imgNotif);
             TextView txtDescriptionNotif = view.findViewById(R.id.txtDescriptionNotif);
             TextView txtTitleNotif = view.findViewById(R.id.txtTitleNotif);
-            Button btnMoreNotif = view.findViewById(R.id.btnMoreNotif);
             Button btnDeleteNotif = view.findViewById(R.id.btnDeleteNotif);
 
 
@@ -164,8 +148,8 @@ public class NotificationsFragment extends Fragment {
                 e.printStackTrace();
             }
             if(userImage == null){
-                userImage = BitmapFactory.decodeResource( getActivity().getApplicationContext().getResources(),
-                        R.drawable.ic_report_black_24dp);
+                userImage = BitmapFactory.decodeResource( getActivity().getApplicationContext()
+                                                .getResources(),R.drawable.ic_report_black_24dp);
             }
             imgNotif.setImageBitmap(userImage);
 
@@ -180,6 +164,36 @@ public class NotificationsFragment extends Fragment {
                 descriptionNotif = "Se ha realizado un nuevo reporte";
             }
 
+            btnDeleteNotif.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(getActivity(), "HOLA222", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+
+            view.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    if(notifications.get(i).isNews()){
+                        int idSearchNews = notifications.get(i).getIdContent();
+                        NotificationsFragment.ExecuteGetListUsersCommunity_by_idNews
+                                executeGetListUsersCommunity_by_idNews = new NotificationsFragment.
+                                ExecuteGetListUsersCommunity_by_idNews(idSearchNews);
+                        executeGetListUsersCommunity_by_idNews.execute();
+                    }
+
+                    else if(notifications.get(i).isEvents()){
+                        Toast.makeText(getActivity(), "aun no cargo eventos", Toast.LENGTH_SHORT).show();
+                    }
+
+                    else if(notifications.get(i).isReports()){
+                        Toast.makeText(getActivity(), "aun no cargo reportes", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            });
 
             txtDescriptionNotif.setText(descriptionNotif);
             txtTitleNotif.setText(notifications.get(i).getTitleContent());
@@ -188,7 +202,7 @@ public class NotificationsFragment extends Fragment {
         }
     }
 
-    /////////////////////////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////////////////////
     public class ExecuteGetNotifications extends AsyncTask<String, Void, String> {
         boolean isOk = false;
 
@@ -204,7 +218,7 @@ public class NotificationsFragment extends Fragment {
             User_Singleton user = User_Singleton.getInstance();
             String[] keys = {"idUser", "auth_token"};
             String[] values = {Integer.toString(user.getId()), user.getAuth_token()};
-            isOk = api.get_delete_base(keys, values, 32, "GET",1);
+            isOk = api.get_delete_base(keys, values, 32,"GET",1);
 
             return null;
         }
@@ -212,7 +226,6 @@ public class NotificationsFragment extends Fragment {
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
-
             if(isOk){
                 loadNotifications(API_Access.getInstance().getJsonObjectResponse());
             }else{
@@ -221,6 +234,209 @@ public class NotificationsFragment extends Fragment {
                 Toast.makeText(getActivity(), mensaje, Toast.LENGTH_SHORT).show();
             }
         }
+    }
+
+
+    //=============================================================================================
+    public class ExecuteGetListUsersCommunity_by_idNews extends AsyncTask<String, Void, String> {
+        boolean isOk = false;
+        int idNews;
+
+        public ExecuteGetListUsersCommunity_by_idNews(int idNews) {
+            this.idNews = idNews;
+        }
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            API_Access api = API_Access.getInstance();
+
+
+            String[] keys = {"idUser", "idNews", "auth_token"};
+            String[] values = {Integer.toString(User_Singleton.getInstance().getId()),
+                           Integer.toString(idNews),  User_Singleton.getInstance().getAuth_token()};
+            isOk = api.get_delete_base(keys, values, 29, "GET", 1);
+
+            return null;
+
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            if(isOk){
+                JSONObject response = API_Access.getInstance().getJsonObjectResponse();
+
+                //set user auth_token
+                try {
+                    String token = response.getString("auth_token");
+                    User_Singleton.getInstance().setAuth_token(token);
+                    LoginAcivity.actualizarAuth_Token(token, getActivity());
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                loadUsersNews(API_Access.getInstance().getJsonObjectResponse());
+                NotificationsFragment.ExecuteGetNewsbyId executeGetNewsbyId = new
+                        NotificationsFragment.ExecuteGetNewsbyId(idNews);
+                executeGetNewsbyId.execute();
+                String mensaje = "Cargando contenido de la difusión";
+                Toast.makeText( getActivity(), mensaje, Toast.LENGTH_SHORT).show();
+
+            }else{
+                String mensaje = "Error al obtener los datos de la difusión";
+                Toast.makeText( getActivity(), mensaje, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void loadUsersNews(JSONObject jsonResult) {
+
+        try {
+            CommunityActivity.listUsers.clear();
+
+            JSONArray jsonUsersList = jsonResult.getJSONArray("usuarios");  //Importante
+            for (int i = 0; i < jsonUsersList.length(); i++) {
+                JSONObject jsonUser = (JSONObject) jsonUsersList.get(i);
+
+                HttpGetBitmap request = new HttpGetBitmap();
+                Bitmap newImage = null;
+                try {
+                    newImage = request.execute(jsonUser.getString("photo")).get();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                }
+
+                int id = jsonUser.getInt("id");
+                String name = jsonUser.getString("name");
+                String email = jsonUser.getString("email");
+                String tel = jsonUser.getString("tel");
+                String cel = jsonUser.getString("cel");
+                String address = jsonUser.getString("address");
+                String url_photo = jsonUser.getString("photo");
+                String url_photo_rounded = jsonUser.getString("photo_thumbnail");
+                Bitmap photo = convertirBitmap(url_photo);
+                Bitmap photo_rounded = convertirBitmap(url_photo_rounded);
+                boolean privateProfile = jsonUser.getBoolean("isPrivate");
+
+                if(photo == null){
+                    photo = BitmapFactory.decodeResource( getActivity().getResources(),
+                            R.drawable.user_box_photo);
+                }
+                if(photo_rounded == null){
+                    photo_rounded = BitmapFactory.decodeResource( getActivity().getResources(),
+                            R.drawable.user_rounded_photo);
+                }
+
+                User userObject = new User(Integer.toString(id),name,email,tel,cel,address,
+                                    url_photo,url_photo_rounded,photo,photo_rounded,privateProfile);
+
+                CommunityActivity.listUsers.add(userObject);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //=============================================================================================
+    public class ExecuteGetNewsbyId extends AsyncTask<String, Void, String> {
+        boolean isOk = false;
+        int idNews;
+
+        public ExecuteGetNewsbyId(int idNews) {
+            this.idNews = idNews;
+        }
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            API_Access api = API_Access.getInstance();
+
+
+            String[] keys = {"idUser", "idNews", "auth_token"};
+            String[] values = {Integer.toString(User_Singleton.getInstance().getId()),
+                    Integer.toString(idNews),  User_Singleton.getInstance().getAuth_token()};
+            isOk = api.get_delete_base(keys, values, 34, "GET", 1);
+
+            return null;
+
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            if(isOk){
+                JSONObject response = API_Access.getInstance().getJsonObjectResponse();
+
+                //set user auth_token
+                try {
+                    String token = response.getString("auth_token");
+                    User_Singleton.getInstance().setAuth_token(token);
+                    LoginAcivity.actualizarAuth_Token(token, getActivity());
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                JSONObject  jsonNews = null;
+                try {
+                    jsonNews = (JSONObject)  API_Access.getInstance().getJsonObjectResponse()
+                                                                            .getJSONObject("news");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                Intent intent = new Intent(getActivity(),NewsMoreActivity.class);
+
+                try {
+                    intent.putExtra("idActual", jsonNews.getInt("id"));
+                    intent.putExtra("Title", jsonNews.getString("title"));
+                    intent.putExtra("DateN", jsonNews.getString("date"));
+                    intent.putExtra("Description", jsonNews.getString("description"));
+                    intent.putExtra("isApproved", jsonNews.getBoolean("approved"));
+                    NewsMoreActivity.fromFavorites = true;
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                startActivity(intent);
+                String mensaje = "Cargando contenido de la difusión";
+                Toast.makeText( getActivity(), mensaje, Toast.LENGTH_SHORT).show();
+
+            }else{
+                String mensaje = "Error al obtener los datos de la difusión";
+                Toast.makeText( getActivity(), mensaje, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+    //=============================================================================================
+
+    private Bitmap convertirBitmap(String url){
+        HttpGetBitmap request = new HttpGetBitmap();
+        Bitmap newImage = null;
+        try {
+            newImage = request.execute(url).get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        return newImage;
     }
 
 
