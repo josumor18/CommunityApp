8d2804592a204eb4b7c7f3ca9d0ff47c2a12f33a
Carlos Josué Morales R <carlosjosum@hotmail.com>
2018-10-11 02:11:03 -0600

    Messages list loaded

--- app/src/main/java/elcarmen/project/community/Business/ChatActivity.java
+++ app/src/main/java/elcarmen/project/community/Business/ChatActivity.java
@@ -1,13 +1,17 @@
 package elcarmen.project.community.Business;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
@@ -15,6 +19,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -36,7 +41,9 @@ public class ChatActivity extends AppCompatActivity {
     private int id_chat;
     private boolean is_group;
     private String user_name;
-    ListView lvChatMessages;
+    //ListView lvChatMessages;
+    RecyclerView lvChatMessages;
+    TextView txtMessage;
 
     ArrayList<Message> messages = new ArrayList<Message>();
 
@@ -60,9 +67,11 @@ public class ChatActivity extends AppCompatActivity {
         getSupportActionBar().setSubtitle(intent.getStringExtra("community_name"));
 
         lvChatMessages = findViewById(R.id.lvChatMessages);
-        lvChatMessages.setDivider(null);
+        lvChatMessages.setLayoutManager(new LinearLayoutManager(this, LinearLayout.VERTICAL, false));
+        //lvChatMessages.setDivider(null);
+        txtMessage = findViewById(R.id.txtMessage);
 
-        ExecuteGetMessages executeGetMessages = new ExecuteGetMessages();
+        ExecuteGetMessages executeGetMessages = new ExecuteGetMessages(true, -1);
         executeGetMessages.execute();
     }
 
@@ -77,17 +86,39 @@ public class ChatActivity extends AppCompatActivity {
     }
 
     public void send_message(View view){
+        String texto = txtMessage.getText().toString();
+        if(!texto.isEmpty()){
+            int last_id = -1;
+            if(!messages.isEmpty()){
+                last_id = messages.get(messages.size() - 1).getId();
+            }
 
-        // setear el atributo "sent" de Message en false
+            // setear el atributo "sent" de Message en false
+            Message newMessage = new Message(-1, id_chat, User_Singleton.getInstance().getId(), texto, false, "2018-10-05T05:32:19.709Z");
+            newMessage.setSent(false);
+
+            messages.add(newMessage);
+
+            //lvChatMessages.setAdapter(new MessagesAdapter());
+            MessagesAdapter messagesAdapter = new MessagesAdapter(messages);
+            lvChatMessages.setAdapter(messagesAdapter);
+            lvChatMessages.scrollToPosition(messages.size()-1);
+
+            ExecuteGetMessages executeGetMessages = new ExecuteGetMessages(false, last_id);
+            executeGetMessages.execute(texto);
+
+        }
     }
 
     private void cargarMessages(JSONObject jsonResult) {
 
+        ArrayList<Message> newMessages = new ArrayList<Message>();
+        int jsonCount_my_messages = 0;
         try {
-            messages.clear();
-            String token = jsonResult.getString("auth_token");
-            User_Singleton.getInstance().setAuth_token(token);
-            LoginAcivity.actualizarAuth_Token(token, getApplicationContext());
+            //messages.clear();
+            //String token = jsonResult.getString("auth_token");
+            //User_Singleton.getInstance().setAuth_token(token);
+            //LoginAcivity.actualizarAuth_Token(token, getApplicationContext());
 
             JSONArray jsonMessagesList = jsonResult.getJSONArray("messages_list");
 
@@ -98,122 +129,155 @@ public class ChatActivity extends AppCompatActivity {
                         jsonMessage.getInt("id_user"), jsonMessage.getString("message"),
                         jsonMessage.getBoolean("seen"), jsonMessage.getString("created_at"));
 
-                messages.add(message);
+                //messages.add(message);
+                newMessages.add(message);
             }
+
+            jsonCount_my_messages = jsonResult.getInt("count_my_messages");
         } catch (JSONException e) {
             e.printStackTrace();
         }
 
-        lvChatMessages.setAdapter(new MessagesAdapter());
+        int count_my_messages = 0;
+        for(Message mes: messages){
+            if(mes.getId_user() == User_Singleton.getInstance().getId()){
+                count_my_messages++;
+            }
+        }
 
+        if(!newMessages.isEmpty()){
+            for(int i = 0; i < messages.size(); i++){
+                if((!messages.get(i).isSent()) && (count_my_messages > jsonCount_my_messages)){
+                    messages.remove(i);
+                }
+            }
+            for(Message m: newMessages){
+                messages.add(m);
+            }
+        }
+        //lvChatMessages.setAdapter(new MessagesAdapter());
+        MessagesAdapter messagesAdapter = new MessagesAdapter(messages);
+        lvChatMessages.setAdapter(messagesAdapter);
+        lvChatMessages.scrollToPosition(messages.size()-1);
     }
 
     /////////////////////////////////////////////////////////////////////////////////////////////////
-    public class MessagesAdapter extends BaseAdapter {
+    public class MessagesAdapter extends RecyclerView.Adapter<MessagesAdapter.ViewHolderDatos>{
 
-        public MessagesAdapter() {
-            super();
+        ArrayList<Message> messageArrayList;
+
+        public MessagesAdapter(ArrayList<Message> messageArrayList) {
+            this.messageArrayList = messageArrayList;
+        }
+
+        @NonNull
+        @Override
+        public ViewHolderDatos onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.chat_item, null, false);
+            return new ViewHolderDatos(view);
         }
 
         @Override
-        public int getCount() {
-            return messages.size();
+        public void onBindViewHolder(@NonNull ViewHolderDatos holder, int position) {
+            holder.asignarDatos(messageArrayList.get(position));
         }
 
         @Override
-        public Object getItem(int i) {
-            return messages.get(i);
+        public int getItemCount() {
+            return messageArrayList.size();
         }
 
-        @Override
-        public long getItemId(int i) {
-            return 0;
-        }
+        public class ViewHolderDatos extends RecyclerView.ViewHolder {
+            TextView txtContactName_Message, txtContactMessage, txtMessageContactDateTime, txtUserMessage, txtMessageUserDateTime;
+            ImageView imgContactMessage, imgClock;
 
-        @Override
-        public View getView(int i, View view, ViewGroup viewGroup) {
-            LayoutInflater inflater = getLayoutInflater();
-            if (view == null) {
-                view = inflater.inflate(R.layout.chat_item, null);
+
+            public ViewHolderDatos(View itemView) {
+                super(itemView);
+                txtContactName_Message = itemView.findViewById(R.id.txtContactName_Message);
+                txtContactMessage = itemView.findViewById(R.id.txtContactMessage);
+                txtMessageContactDateTime = itemView.findViewById(R.id.txtMessageContactDateTime);
+                txtUserMessage = itemView.findViewById(R.id.txtUserMessage);
+                txtMessageUserDateTime = itemView.findViewById(R.id.txtMessageUserDateTime);
+                txtMessageUserDateTime = itemView.findViewById(R.id.txtMessageUserDateTime);
+                imgContactMessage = itemView.findViewById(R.id.imgContactMessage);
+                imgClock = itemView.findViewById(R.id.imgClock);
             }
 
-            ImageView imgContactMessage = view.findViewById(R.id.imgContactMessage);
-            TextView txtContactName_Message = view.findViewById(R.id.txtContactName_Message);
-            TextView txtContactMessage = view.findViewById(R.id.txtContactMessage);
-            TextView txtMessageContactDateTime = view.findViewById(R.id.txtMessageContactDateTime);
-            TextView txtUserMessage = view.findViewById(R.id.txtUserMessage);
-            TextView txtMessageUserDateTime = view.findViewById(R.id.txtMessageUserDateTime);
-            ImageView imgClock = view.findViewById(R.id.imgClock);
-
-            ArrayList<User> users = CommunityActivity.listUsers;
-            if(is_group){
-                for(User u: users){
-                    if(Integer.parseInt(u.getId()) == messages.get(i).getId_user()){
-                        user_name = u.getName();
+            public void asignarDatos(Message message) {
+                ArrayList<User> users = CommunityActivity.listUsers;
+                if(is_group){
+                    for(User u: users){
+                        if(Integer.parseInt(u.getId()) == message.getId_user()){
+                            user_name = u.getName();
+                        }
                     }
                 }
-            }
 
-            if(messages.get(i).getId_user() == User_Singleton.getInstance().getId()){
-                txtUserMessage.setText(messages.get(i).getMessage());
-                if(messages.get(i).isSent()){
-                    txtUserMessage.setBackground(getResources().getDrawable(R.drawable.rounded_user_message));
-                    txtMessageUserDateTime.setText(messages.get(i).getCreated_at_to_String());
-                    imgClock.setVisibility(View.GONE);
-                }else{
+                if(message.getId_user() == User_Singleton.getInstance().getId()){
+                    txtUserMessage.setText(message.getMessage());
+                    if(message.isSent()){
+                        txtUserMessage.setBackground(getResources().getDrawable(R.drawable.rounded_user_message));
+                        txtMessageUserDateTime.setText(message.getCreated_at_to_String());
+                        imgClock.setVisibility(View.GONE);
+                    }else{
+                        txtMessageUserDateTime.setVisibility(View.GONE);
+                    }
+                    int paddingPx = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 10, getResources().getDisplayMetrics());
+                    txtUserMessage.setPadding(paddingPx, paddingPx, paddingPx, paddingPx);
+
+                    imgContactMessage.setVisibility(View.GONE);
+                    txtContactName_Message.setVisibility(View.GONE);
+                    txtContactMessage.setVisibility(View.GONE);
+                    txtMessageContactDateTime.setVisibility(View.GONE);
+                }else {
+                    String link_photo = "";
+                    for (User u : users) {
+                        if (Integer.parseInt(u.getId()) == message.getId_user()) {
+                            link_photo = u.getUrl_photo_rounded();
+                        }
+                    }
+
+                    HttpGetBitmap request = new HttpGetBitmap();
+                    Bitmap userImage = null;
+                    if (!link_photo.isEmpty()) {
+                        try {
+                            userImage = request.execute(link_photo).get();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        } catch (ExecutionException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    if (userImage == null) {
+                        userImage = BitmapFactory.decodeResource(getApplicationContext().getResources(),
+                                R.drawable.user_rounded_photo);
+                    }
+                    imgContactMessage.setImageBitmap(userImage);
+
+                    txtContactName_Message.setText(user_name);
+                    txtContactMessage.setText(message.getMessage());
+                    txtMessageContactDateTime.setText(message.getCreated_at_to_String());
+
+                    txtUserMessage.setVisibility(View.GONE);
                     txtMessageUserDateTime.setVisibility(View.GONE);
-
+                    imgClock.setVisibility(View.GONE);
                 }
-
-                imgContactMessage.setVisibility(View.GONE);
-                txtContactName_Message.setVisibility(View.GONE);
-                txtContactMessage.setVisibility(View.GONE);
-                txtMessageContactDateTime.setVisibility(View.GONE);
-            }else{
-                String link_photo = "";
-                for(User u: users){
-                    if(Integer.parseInt(u.getId()) == messages.get(i).getId_user()){
-                        link_photo = u.getUrl_photo_rounded();
-                    }
-                }
-
-                HttpGetBitmap request = new HttpGetBitmap();
-                Bitmap userImage = null;
-                if(!link_photo.isEmpty()){
-                    try {
-                        userImage = request.execute(link_photo).get();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    } catch (ExecutionException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-                if(userImage == null){
-                    userImage = BitmapFactory.decodeResource( getApplicationContext().getResources(),
-                            R.drawable.user_rounded_photo);
-                }
-                imgContactMessage.setImageBitmap(userImage);
-
-                txtContactName_Message.setText(user_name);
-                txtContactMessage.setText(messages.get(i).getMessage());
-                txtMessageContactDateTime.setText(messages.get(i).getCreated_at_to_String());
-
-                txtUserMessage.setVisibility(View.GONE);
-                txtMessageUserDateTime.setVisibility(View.GONE);
-                imgClock.setVisibility(View.GONE);
             }
-
-            return view;
         }
     }
-
 
     /////////////////////////////////////////////////////////////////////////////////////////////////
     public class ExecuteGetMessages extends AsyncTask<String, Void, String> {
         boolean isOk = false;
+        boolean forGet = false;
+        int last_id = -1;
 
-        public ExecuteGetMessages() {
+        public ExecuteGetMessages(boolean forGet, int last_id) {
+            this.forGet = forGet;
+            this.last_id = last_id;
         }
 
         @Override
@@ -225,13 +289,17 @@ public class ChatActivity extends AppCompatActivity {
         protected String doInBackground(String... strings) {
             API_Access api = API_Access.getInstance();
             User_Singleton user = User_Singleton.getInstance();
-            String[] keys = {"id", "auth_token", "id_chat", "last_id"};
-            int last_id = -1;
-            if(messages.size() > 0){
-                last_id = messages.get(messages.size()-1).getId();
+            if(forGet){
+                String[] keys = {"id", "auth_token", "id_chat", "last_id"};
+                String[] values = {Integer.toString(user.getId()), user.getAuth_token(), Integer.toString(id_chat), Integer.toString(last_id)};
+                isOk = api.get_delete_base(keys, values, 37, "GET",1);
+            }else{
+                String[] keys = {"id", "auth_token", "id_chat", "message"};
+                String[] values = {Integer.toString(user.getId()), user.getAuth_token(), Integer.toString(id_chat), strings[0]};
+                isOk = api.post_put_base(keys, values, 36, "POST",1);
             }
-            String[] values = {Integer.toString(user.getId()), user.getAuth_token(), Integer.toString(id_chat), Integer.toString(last_id)};
-            isOk = api.get_delete_base(keys, values, 37, "GET",1);
+
+
 
             return null;
         }
@@ -241,7 +309,13 @@ public class ChatActivity extends AppCompatActivity {
             super.onPostExecute(s);
 
             if(isOk){
-                cargarMessages(API_Access.getInstance().getJsonObjectResponse());
+                if(forGet){
+                    cargarMessages(API_Access.getInstance().getJsonObjectResponse());
+                }else{
+                    txtMessage.setText("");
+                    //ExecuteGetMessages executeGetMessages = new ExecuteGetMessages(true, last_id);
+                    //executeGetMessages.execute();
+                }
             }else{
                 String mensaje = "Error al obtener los chats";
 
